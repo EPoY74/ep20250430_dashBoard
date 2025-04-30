4ac1fc1813723201ce401d9aa5fd92eed25e85ae
EugeniiPetrov <epoy74@gmail.com>
2025-04-30 21:13:37 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+20250430 Пишу дашбоард для серверов видеонаблюдения
+трассир.
+Автор: Евгений Петров
+e-mail: p174@mail.ru
+"""
+
+import streamlit as st
+
+if __name__ == "__main__":
+    st.write('Hello World!!!')
