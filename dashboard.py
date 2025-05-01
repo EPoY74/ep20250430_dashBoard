e98c780c5012573faf354014b0a44f6939ef1bdc
EugeniiPetrov <epoy74@gmail.com>
2025-05-01 21:50:28 +0500

    initial project. Saving

--- dashboard.py
+++ dashboard.py
@@ -2,9 +2,14 @@
 Дашбоард от AI болванка
 """
 
+from typing import Any
 import streamlit as st
 import pandas as pd
 import time
+
+
+DASHBOARD_COLUNNS = 4
+
 
 def main() -> None:
     """
@@ -12,23 +17,70 @@ def main() -> None:
     """
 
     st.title("Мониторинг видеонаблюдения")
-    
-    #  Заглушка данных
-    servers = [
-        {"name": "Server1", "cpu": 45, "status": "online"},
-        {"name": "Server2", "cpu": 64, "status": "online"},
-        {"name": "Server3", "cpu": 80, "status": "online"},
-        {"name": "Server4", "cpu": 38, "status": "Offline"},
-    ]
 
-    while True:
-        df = pd.DataFrame(servers)
-        # st.table(df)
-        st.dataframe(
-            df
+    def fetch_serevers_data() -> list[dict[str, Any | None]]:
+        """
+        Заглушка для данных.
+        """
+        servers = [
+            {"name": "Server1", "cpu": 45, "status": "online"},
+            {"name": "Server2", "cpu": 64, "status": "online"},
+            {"name": "Server3", "cpu": 80, "status": "online"},
+            {"name": "Server4", "cpu": 38, "status": "offline"},
+            {"name": "Server1-1", "cpu": 45, "status": "online"},
+            {"name": "Server2-3", "cpu": 64, "status": "online"},
+            {"name": "Server3-2", "cpu": 80, "status": "online"},
+            {"name": "Server4-1", "cpu": 38, "status": "offline"}
+        ]
+        return servers
+
+    # Стиль для карточек серверов
+    st.markdown(
+        """
+        <style>
+        .server-card {
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            padding: 15px;
+            margin: 10px;
+            width: 300px;
+            display: inline-block;
+            vertical-align: top;
+            box-sizing: border-box; 
+        }
+        .online { color: green; }
+        .offline { color: red; }
+        </style>
+    """,
+        unsafe_allow_html=True,
+    )
+    servers_health = fetch_serevers_data()
+
+    view_columns_per_row = st.columns(
+        DASHBOARD_COLUNNS,
+        gap="medium",
+        vertical_alignment="center"
+        )
+
+    for i, server in enumerate(servers_health):
+        with view_columns_per_row[i % DASHBOARD_COLUNNS]:
+            st.markdown(
+                f"""
+                    <div class="server-card">
+                    <h3>{server["name"]}</h3>
+                    <p>CPU: {server["cpu"]}</p>
+                    <p>Статус: <span class="{server["status"]}">{server["status"]}</span></p>
+                    </div>
+                """,
+                unsafe_allow_html=True,
             )
-        time.sleep(10)
-        st.rerun()
+
+    # while True:
+    #     df = pd.DataFrame(servers)
+    #     # st.table(df)
+    #     st.dataframe(df)
+    #     time.sleep(10)
+    #     st.rerun()
 
 
 if __name__ == "__main__":
