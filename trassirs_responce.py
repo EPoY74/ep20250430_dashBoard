d52311d67587eaee6801c37c4067c29040d584cb
EugeniiPetrov <epoy74@gmail.com>
2025-05-05 08:54:20 +0500

    Пример json  с ответом от сервера

---
+++ trassirs_responce.py
@@ -0,0 +1,18 @@
+{
+    "disks": "1",
+    "database": "1",
+    "channels_total": "26",
+    "channels_online": "26",
+    "uptime": "421839",
+    "cpu_load": "32.89",
+    "network": "1",
+    "automation": "1",
+    "disks_stat_main_days": "52.12",
+    "disks_stat_priv_days": "0.00",
+    "disks_stat_subs_days": "14.42",
+}
+
+Шаг1
+https://10.1.15.115:8080/login?username=***&password=***
+Шаг2
+https://10.1.15.115:8080/health?sid=tQYrWWmt
