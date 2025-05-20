d0589da7abe78d169fe9a78bc7f3005b2e36a4b0
EugeniiPetrov <epoy74@gmail.com>
2025-05-20 16:55:03 +0500

    Добавил json  ответ сервера о здоровье

--- trassirs_responce.py
+++ trassirs_responce.py
@@ -20,3 +20,20 @@ https://10.1.15.115:8080/health?sid=tQYrWWmt # Запрос здоровья
 https://10.1.15.115:8080/settings/network_interfaces/hostname?sid=EEPnwWL4 # Запрос названия сервера
 
 https://10.1.15.115:8080/settings/?sid=EEPnwWL4 # Запрос  настроек сервера
+
+
+
+Ответ о здоровье сервера:
+{
+	"disks": "1",
+	"database": "1",
+	"channels_total": "27",
+	"channels_online": "27",
+	"uptime": "370733",
+	"cpu_load": "48.72",
+	"network": "1",
+	"automation": "1",
+	"disks_stat_main_days": "32.00",
+	"disks_stat_priv_days": "0.00",
+	"disks_stat_subs_days": "14.05"
+}
