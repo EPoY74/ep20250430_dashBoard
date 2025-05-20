ff85088776169a164b291785704d18e2f91f9dc9
EugeniiPetrov <epoy74@gmail.com>
2025-05-20 08:51:42 +0500

    Initial commit. Script is not work

---
+++ dashboard_project/servers_monitor.py
@@ -0,0 +1,19 @@
+import dotenv
+
+
+class ServerAuthDataLoader:
+    '''
+    Загружаю данные для авторизации на серверах
+    '''
+    def __init__(self) -> None:
+        """
+        Загрузка данных из файла
+        :param self: Description
+        :type self: 
+        """
+        
+
+
+
+if __name__ == "__main__":
+    pass
