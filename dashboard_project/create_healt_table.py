ff18fac2270c928aca3f36eaab93e5a30b51ce23
EugeniiPetrov <epoy74@gmail.com>
2025-05-20 16:55:46 +0500

    Начало: sql запрос на создание таблицу в БД

---
+++ dashboard_project/create_healt_table.py
@@ -0,0 +1,23 @@
+
+
+CREATE TABLE server_health (
+    id SERIAL PRIMARY KEY,
+    ip TEXT NOT NULL,
+    timestamp TIMESTAMPTZ DEFAULT now(),
+
+    channels_total INTEGER,
+    channels_online INTEGER,
+    cpu_load REAL,
+    uptime INTEGER,
+
+    disks_ok BOOLEAN,
+    database_ok BOOLEAN,
+    network_ok BOOLEAN,
+    automation_ok BOOLEAN,
+
+    disks_stat_main_days REAL,
+    disks_stat_priv_days REAL,
+    disks_stat_subs_days REAL,
+
+    raw JSONB
+);
