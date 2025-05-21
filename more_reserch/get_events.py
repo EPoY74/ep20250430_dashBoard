3dc9a18fd714f45dd9217bfa781c028e85b99c93
Eugenii B. Petrov <epoy74@gmail.com>
2025-05-21 16:40:06 +0500

    Поправил неприятности с файле, почему-то код работал с ошибками, хотя ниченго не поменялось

--- more_reserch/get_events.py
+++ more_reserch/get_events.py
@@ -7,43 +7,54 @@
 import logging
 import time
 
+import aiohttp
 import dotenv
 import requests
 
 logging.basicConfig(
     level=logging.DEBUG,  # Уровень логгирования
     format="%(asctime)s - %(levelname)s - %(message)s",  # формат
-    # filename='app.log',                      # если хочешь писать в файл
-    # filemode='a'                             # 'a' — добавлять, 'w' — перезаписывать
+    
+    # если хочешь писать в файл
+    # filename='app.log',
+    
+    # 'a' — добавлять, 'w' — перезаписывать
+    # filemode='a'
 )
 
 
 dotenv.load_dotenv()
 
-login_url = "https://10.1.15.100:8080/login"
-events_url = "https://10.1.15.100:8080/health"
+LOGIN_URL = "https://10.1.15.100:8080/login"
+EVENTS_URL = "https://10.1.15.100:8080/health"
 
 # Авторизация
 resp = requests.get(
-    login_url,
-    params={"username": "****", "password": "****"},
+    LOGIN_URL,
+    params={"username": "admin", "password": "7905520Tr"},
     verify=False,
+    timeout=5,
 )
 logging.debug("Login response code: %s", resp.status_code)
 logging.debug("Login response body: %s", resp.text)
 input()
 
 sid = resp.json().get("sid")
-logging.debug(f"My session: {sid}")
+logging.debug("My session: %s", sid)
 
 
 # Получение событий
 while True:
     try:
-        r = requests.get(events_url, params={"sid": sid}, verify=False)
+        r = requests.get(
+            EVENTS_URL,
+            params={"sid": sid},
+            verify=False,
+            timeout=5,
+        )
         logging.info(r.text)
     except Exception as e:
-        logging.error("Error:", e)
+        logging.error("Error: %s", str(e))
         raise
     input()
     time.sleep(1)  # задержка между запросами
