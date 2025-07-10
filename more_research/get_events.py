f77d70333724ec56a1c8d5476d6f4fc5f5a0955c
Eugenii B. Petrov <epoy74@gmail.com>
2025-07-10 23:01:18 +0500

    Сохранение

--- more_research/get_events.py
+++ more_research/get_events.py
@@ -12,50 +12,86 @@ import aiohttp
 import dotenv
 import requests
 
-logging.basicConfig(
-    level=logging.DEBUG,  # Уровень логгирования
-    format="%(asctime)s - %(levelname)s - %(message)s",  # формат
-    # если хочешь писать в файл
-    # filename='app.log',
-    # 'a' — добавлять, 'w' — перезаписывать
-    # filemode='a'
-)
 
+def init_logger():
+    """
+    Инициализирует логгер для дальнейшей работы
+    """
+
+    logging.basicConfig(
+        level=logging.DEBUG,  # Уровень логгирования
+        format="%(asctime)s - %(levelname)s - %(message)s",  # формат
+        # если хочешь писать в файл
+        # filename='app.log',
+        # 'a' — добавлять, 'w' — перезаписывать
+        # filemode='a'
+    ) # задержка между запросами
+
+
+init_logger()
 
 dotenv.load_dotenv()
-
+# DVR_USER_NAME: str | None = None
+# print_to_user: str | None = "Ошибок к выводу в логгер не было."
 LOGIN_URL = os.getenv("LOGIN_URL")
 EVENTS_URL = os.getenv("EVENTS_URL")
 DVR_USER_NAME = os.getenv("DVR_USER_NAME")
+if DVR_USER_NAME is None:
+    print_to_user = (
+        "Переменная окружения не найдена. "
+        "Задайте переменную окружения DVR_USER_NAME"
+    )
+    logging.error(print_to_user)
+    exit(print_to_user)
+elif DVR_USER_NAME == "":
+    print_to_user = (
+        "Переменная океружения DVR_USER_NAME пустая. "
+        "Задайте значение для данной переменной"
+    )
+    logging.error(print_to_user)
+    exit(print_to_user)
 DVR_PASSWORD = os.getenv("DVR_PASSWORD")
 
-# Авторизация
-resp = requests.get(
-    LOGIN_URL,
-    params={"username": DVR_USER_NAME, "password": DVR_PASSWORD},
-    verify=False,
-    timeout=5,
-)
-logging.debug("Login response code: %s", resp.status_code)
-logging.debug("Login response body: %s", resp.text)
-input()
 
-sid = resp.json().get("sid")
-logging.debug("My session: %s", sid)
+def main():
+    """
+    Точка входа в скрипт
+    """
 
 
-# Получение событий
-while True:
-    try:
-        r = requests.get(
-            EVENTS_URL,
-            params={"sid": sid},
-            verify=False,
-            timeout=5,
-        )
-        logging.info(r.text)
-    except Exception as e:
-        logging.error("Error: %s", str(e))
-        raise
+
+    # Авторизация
+    resp = requests.get(
+        LOGIN_URL,
+        params={"username": DVR_USER_NAME, "password": DVR_PASSWORD},
+        verify=False,
+        timeout=5,
+    )
+    logging.debug("Login response code: %s", resp.status_code)
+    logging.debug("Login response body: %s", resp.text)
     input()
-    time.sleep(1)  # задержка между запросами
+
+    sid = resp.json().get("sid")
+    logging.debug("My session: %s", sid)
+
+
+    # Получение событий
+    while True:
+        try:
+            r = requests.get(
+                EVENTS_URL,
+                params={"sid": sid},
+                verify=False,
+                timeout=5,
+            )
+            logging.info(r.text)
+        except Exception as e:
+            logging.error("Error: %s", str(e))
+            raise
+        input()
+        time.sleep(1) 
+
+
+
+if __name__ ==  "__main__":
+    main()
