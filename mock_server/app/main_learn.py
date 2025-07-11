8c2c09ef4bf3c256c628da53c10b7c693b91f5a2
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-07-11 17:04:39 +0500

    Более корректное наименование - так как  это не сам сервер, а подготовка к нему

--- mock_server/app/main_learn.py
+++ mock_server/app/main_learn.py
@@ -1,31 +1,33 @@
 """
-Исследователькой программирование: Пишу моковый сервер для 
+Исследователькой программирование: Пишу моковый сервер для
 тестирования дашбоарда дла dar trassir
 """
 
 from fastapi import FastAPI
 
-from app.models import Item
+from mock_server.app.models_learn import Item
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    '''
+    """
     Основной обработчик
-    '''
-    return{"message": "Hello fastapi"}
+    """
+    return {"message": "Hello fastapi"}
+
 
 @app.get("/hello")
 def say_hello(name: str = "Гость"):
     """
     Приветствует гостя.
 
-    Аргуметнты: 
+    Аргуметнты:
     name(str): Имя гостя, которого приветсвуем
     """
     return {"message": f"Привет, {name}!"}
+
 
 @app.get("/item/{item_id}")
 def read_item(item_id: int, detail: bool | None = False):
@@ -35,18 +37,12 @@ def read_item(item_id: int, detail: bool | None = False):
     Аргументы:
     item_id(int): номер объекта
     """
-    return {
-        "item_id": item_id, 
-        "detail" : detail 
-        }
+    return {"item_id": item_id, "detail": detail}
+
 
 @app.post("/items/")
 def create_item(item: Item):
     """
-    Создаю объект 
+    Создаю объект
     """
-    return {
-        "message" : f"Товар {item.name} создан!",
-        "data": item
-
-    }
+    return {"message": f"Товар {item.name} создан!", "data": item}
