b3be6a1a38976f6bf7a2f64b88d691c2e2491016
EugeniiPetrov@privelegiya <epoy74@gmail.com>
2025-07-25 13:23:30 +0500

    Проба чтения информации

---
+++ dash_202507_last/get_info_from_dvr.py
@@ -0,0 +1,19 @@
+"""
+Файл для  подключения к dvr trassir
+Автор: Евгений Петров
+Почта: p174@mail.ru
+Почта: epoy74@gmail.com
+Телефон: +7 952 517 4228
+"""
+
+from dvr_lib import TrassirConnector
+
+tr15 = TrassirConnector("10.1.15.115")
+
+try:
+    if tr15.identification("admin", "7905520Tr"):
+        print("Подключение к серверу успешно")
+    else:
+        print("подключение к серверу не удалось")
+except Exception as err:
+    raise err
